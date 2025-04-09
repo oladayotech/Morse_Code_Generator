a370c1157b97b68717f7de2de86b9214f91115ec
oladayotech <d1arogundade@gmail.com>
2025-04-09 22:18:12 +0100

    adjusting former code

--- Morse code generator.py
+++ Morse code generator.py
@@ -28,45 +28,54 @@ def type_text(text, delay=0.04):
         time.sleep(delay)
     print()
 
-while True:
-    def action_selection():
-        type_text("""Type ETM to Covert English to Morse Code
+def action_selection():
+    type_text("""Type ETM to Covert English to Morse Code
 Type MTE to Covert Morse Code to English
         """)
     
-        while True:
-            try:
-                user_input = input("").strip().lower()
-                if user_input == "mte" or user_input == "etm":
-                    return user_input
-                else:
-                    type_text("Please select one of the above")
-            except Exception as e:
-                print("e")
+    while True:
+        try:
+            user_input_action_selection = input("").strip().lower()
+            if user_input_action_selection == "mte":
+                user_input_mte = input("Enter Something in Morse code to convert to English: ").split(" ")
+                return user_input_action_selection, user_input_mte
+            elif user_input_action_selection == "etm":
+                user_input_etm = input("Enter Something in English to convert to Morse code: ")
+                return user_input_action_selection, user_input_etm
+            else:
+                type_text("Please select one of the above")
+        except Exception as e:
+            print("e")
     
-    def input_validation():
-        action = action_selection()
-        if action == "mte":
-            user_input = input("Enter Something in Morse code to convert to English").split(" ")
-            for values_in_user_input in user_input:
-                for key, value in morse_code_dict.items():
-                    if value == values_in_user_input:
-                        print(key, end="")
-                        break
+def input_validation(action, user_input):
+    if action == "mte":
+        generated_sentence = []
+        for values_in_user_input in user_input:
+            for key, value in morse_code_dict.items():
+                if value == values_in_user_input:
+                    generated_sentence.append(key)
+                    break
+        return generated_sentence
                         
-        elif action == "etm":
-            user_input = input("Enter Something in English to convert to Morse code")
-            for values in user_input:
-                print(morse_code_dict[values], end=" ")
+    elif action == "etm":
+        generated_morse_code = []
+        for values in user_input:
+            generated_morse_code.append(morse_code_dict[values])
+        return generated_morse_code
+            # print(morse_code_dict[values], end=" ")
     
-    def main():
-        input_validation()
+def main():
+    action, user_input = action_selection()
+    generated_code = input_validation(action, user_input)
+    for morse_code in generated_code:
+        print(morse_code, end=" ")
     
-    if __name__ == "__main__":
+if __name__ == "__main__":
+    while True:
         main()
 
-    play_again = input("Do you want to play again (yes/no): ")
+        play_again = input("\nDo you want to play again (yes/no): ")
 
-    if play_again.lower() != "yes":
-        type_text("Thanks for playing")
-        break
+        if play_again.lower() != "yes":
+            type_text("Thanks for playing")
+            break
